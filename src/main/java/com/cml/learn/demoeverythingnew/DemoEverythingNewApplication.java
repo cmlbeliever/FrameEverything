80db48ecb950fd1703775b21b098cb62ba22a59e
admin <865517964@qq.com>
2020-10-17 19:23:42 +0800

    init

---
+++ src/main/java/com/cml/learn/demoeverythingnew/DemoEverythingNewApplication.java
@@ -0,0 +1,13 @@
+package com.cml.learn.demoeverythingnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEverythingNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEverythingNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cml/learn/demoeverythingnew/DemoEverythingNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cml.learn.demoeverythingnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEverythingNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
